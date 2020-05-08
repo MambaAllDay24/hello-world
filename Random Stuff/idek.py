5793a1fb1eeaa449f8e3fc8f8770d59b267f661a
MambaAllDay24 <63523632+MambaAllDay24@users.noreply.github.com>
2020-05-08 08:45:28 -0500

    Create idek.py

---
+++ Random Stuff/idek.py
@@ -0,0 +1,12 @@
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+for x in range(0,4):
+    t.right(90)
+    t.forward(100)
+
+for x in range(0,1):
+    t.right(225)
+    t.forward(100)
+    t.left(315)
+    t.forward(100)
